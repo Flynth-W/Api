036061874d7ff23dc16cea93d689b79abf804d57
flynth <flynth.dev@gmail.com>
2022-11-10 21:35:00 -0400

    Change fn Account -> Model

--- models/user/checks/Checks.ts
+++ models/user/checks/Checks.ts
@@ -1,12 +1,12 @@
 import { _name } from "./name.ts";
 import { _age } from "./age.ts";
 import { _password } from "./password.ts";
-import { _account } from "./account.ts";
+import { _model } from "./model.ts";
 
 export class Checks {
     
     static Name=_name
     static Age=_age
     static Password=_password
-    static Account=_account
+    static Model=_model
 }
