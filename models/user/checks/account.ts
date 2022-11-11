2b248aa9d69fc5a7974cfd1fcb32e1e6e3deef47
flynth <flynth.dev@gmail.com>
2022-11-10 21:09:28 -0400

    Include account.ts for Model accounts.ts

---
+++ models/user/checks/account.ts
@@ -0,0 +1,10 @@
+import type { response  } from "../../../deps.ts"
+
+export function _account(model:any){
+    return (x:Record<never,never>,mtd?:string):response=>{
+        if( mtd === "post" ){   return model.Post(x)    }
+        if( mtd === "put" ){   return model.Put(x)    }
+        return {body: x ,statusText:"error ",ok:true}
+    }
+}
+
