89dc266e9936982cb5eb096b9a28aa0d3c404f83
flynth <flynth.dev@gmail.com>
2022-11-10 21:13:07 -0400

    Include fn( Password & Account) to Checks.ts

--- models/user/checks/Checks.ts
+++ models/user/checks/Checks.ts
@@ -1,8 +1,12 @@
 import { _name } from "./name.ts";
 import { _age } from "./age.ts";
+import { _password } from "./password.ts";
+import { _account } from "./account.ts";
 
 export class Checks {
     
     static Name=_name
     static Age=_age
+    static Password=_password
+    static Account=_account
 }
