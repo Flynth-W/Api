04a09c2452b4ed6a3a2bedd6c9ceb8bf59c6123a
flynth <flynth.dev@gmail.com>
2022-11-10 21:11:00 -0400

    Include  check/password.ts for Model account.ts

---
+++ models/user/checks/password.ts
@@ -0,0 +1,12 @@
+import type { response  } from "../../../deps.ts"
+
+export function _password(length:{min:number,max:number}){
+    return (x:string):response=>{
+        const xLength=x.length
+        if( length.min <= xLength && xLength <= length.max ){
+            return {body: x ,statusText:"OK",ok:true}
+        }
+        return {body: x ,statusText:"Out to limits",ok:false}
+    }
+}
+
