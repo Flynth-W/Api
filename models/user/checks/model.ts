4838adf192954b2815c2b9457c97a2ffa0e8caa8
flynth <flynth.dev@gmail.com>
2022-11-10 21:34:22 -0400

    Move account.ts -> model.ts

--- models/user/checks/model.ts
+++ models/user/checks/model.ts
@@ -1,6 +1,6 @@
 import type { response  } from "../../../deps.ts"
 
-export function _account(model:any){
+export function _model(model:any){
     return (x:Record<never,never>,mtd?:string):response=>{
         if( mtd === "post" ){   return model.Post(x)    }
         if( mtd === "put" ){   return model.Put(x)    }
