557f6ce13a6b0c3dfa3d98d97ecbcf63bf1f9e9e
flynth <flynth.dev@gmail.com>
2022-11-16 17:14:10 -0400

    Delete collection account

--- db/user/account.ts
+++
@@ -1,9 +0,0 @@
-import { Connect  } from "../../deps.ts"
-// connect to mongodb
-const data={
-    urlmongo:"mongodb://127.0.0.1:8011",
-    dbs:"user",
-    clltn:"account"
-}
-
-export const accountClltn=await Connect(data.urlmongo,data.dbs,data.clltn)
