4f0e04e52aa02d0e3b6551e06eb8a16759069d52
flynth <flynth.dev@gmail.com>
2022-11-17 11:19:02 -0400

    Include :  prop nick in model

--- model/user/profile.ts
+++ model/user/profile.ts
@@ -5,12 +5,15 @@ import { Checks } from "../checks/Checks.ts"
 export type typeProfile={
     name?:property | string
     age?:property|number
+    nick?:property|number
 }
 
 
 const _modelProfile:typeProfile={
     name:{ check:Checks.String(0,15)},
     age:{ check:Checks.Number(18,35)},
+    nick:{check:Checks.Nick}
+
 }
 
 export const UserProfile= new Model(_modelProfile)
