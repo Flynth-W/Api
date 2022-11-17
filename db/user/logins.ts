440e6b6c80af9edfd6ae3142a59c911cfd16bf7a
flynth <flynth.dev@gmail.com>
2022-11-17 11:29:56 -0400

    Delete: db.data & db.logins

--- db/user/logins.ts
+++
@@ -1,9 +0,0 @@
-import { Connect  } from "../../deps.ts"
-// connect to mongodb
-const data={
-    urlmongo:"mongodb://127.0.0.1:8011",
-    dbs:"user",
-    clltn:"logins"
-}
-
-export const logins=await Connect(data.urlmongo,data.dbs,data.clltn)
