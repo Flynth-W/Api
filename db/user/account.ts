3126097b68c8bda8cf35052da2854b73fa6f662f
flynth <flynth.dev@gmail.com>
2022-11-17 11:28:27 -0400

    Include: db.account

---
+++ db/user/account.ts
@@ -0,0 +1,9 @@
+import { Connect  } from "../../deps.ts"
+// connect to mongodb
+const data={
+    urlmongo:"mongodb://127.0.0.1:8011",
+    dbs:"user",
+    clltn:"account"
+}
+
+export const account=await Connect(data.urlmongo,data.dbs,data.clltn)
