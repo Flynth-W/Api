f2445b8fa8397cd01ca114ccc9e90c74ab1015f3
flynth <flynth.dev@gmail.com>
2022-11-18 11:09:37 -0400

    Include: djwt DenoJsonWebToken

--- deps.ts
+++ deps.ts
@@ -5,3 +5,7 @@ export type { response } from "../libAPI/mods.ts"
 
     import file from "./.env.json" assert { type: "json" }
 export const environmet = {...file}
+
+export  { create, verify , getNumericDate, } from "https://deno.land/x/djwt@v2.4/mod.ts";
+export type {  Payload, Header} from "https://deno.land/x/djwt@v2.4/mod.ts";
+
