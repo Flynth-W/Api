3a7fec5ebcd8e492b21c8ae6c5863658d8a5a707
flynth <flynth.dev@gmail.com>
2022-11-18 18:04:15 -0400

    Test: jwt
    create: ok
    verify: ok

--- service/jwt/main.ts
+++ service/jwt/main.ts
@@ -5,5 +5,7 @@ export class DJWT{
     static Create=_create
     static Verify=_verify
 }
-//console.log( await  DJWT.Create({name:"jhuan",pass:"sksldik"}).body )
-//console.log( await DJWT.Verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib2R5Ijp7Im5hbWUiOiJqaHVhbiIsInBhc3MiOiJza3NsZGlrIn19.87WLXpvuvMKphPQLUNGLLOTmEL4CW9BAQgkQ03Ub36g") )
+//const  jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2Njg4MDUwNjcsImJvZHkiOnsibmFtZSI6InVzZXIzIiwicGFzcyI6Im5vcmVhZHVzZXIifX0.trQMKPxWWH87Sf5MnEfVeFmooHx3XpcTQf6SGcbVeZE"
+//console.log( await DJWT.Verify(jwt) )
+
+//console.log( await  DJWT.Create({name:"user3",password:"noreaduser"}).body )
