612597ba7ca23680d225866738b3daa6c9fdbe77
flynth <flynth.dev@gmail.com>
2022-11-17 20:30:54 -0400

    Change: case Sign to case account

--- routes/user/index.ts
+++ routes/user/index.ts
@@ -1,11 +1,11 @@
 import { Profile } from "../../controllers/user/profile/main.ts"
-import { Sign } from "../../controllers/user/sign/main.ts"
+import { Account } from "../../controllers/user/account/main.ts"
 export function userRoute(_routes:string[] , req:Request){
     switch ( _routes[0] ){
         case "profile" :
             return Profile( _routes[1] , req )
-        case "sign" :
-            return Sign( _routes[1] , req )
+        case "account" :
+            return Account( _routes[1] , req )
         default :
             return new Response("asd", { status: 202 });
     }
