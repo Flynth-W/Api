38c719fa05187d395973c0371e5b5ab61cfed12f
flynth <flynth.dev@gmail.com>
2022-11-18 20:09:01 -0400

    Modify: body.user to headers.user

--- controllers/user/account/login/main.ts
+++ controllers/user/account/login/main.ts
@@ -3,11 +3,11 @@ import { _user } from "./user.ts"
 export async function _login(req:Request){
     try{
         const body = await req.json()
-        switch (body.user){
+        const user = req.headers.get("user")
+        switch (user){
             case "user":
                 return _user(body.account)
         }
-        console.log(body.user)
         return new Response("asd", { status: 202 });
 
     }catch(_e){
--- controllers/user/account/login/user.ts
+++ controllers/user/account/login/user.ts
@@ -5,9 +5,9 @@ import { Responses } from "../../../../service/responses/main.ts"
 export async function _user(account:any){
     const resp = await  UserDB.account.findOne(account)
     if( resp === undefined ){ return Responses.Json({error:"user not registered"},406) }
-    
+       
     const jwt= DJWT.Create({nick:resp.nick})
-    if( ! jwt.ok ){ return Responses.Json({jwt},  401 ) }
+    if( ! jwt.ok ){ return Responses.Json(jwt,  401 ) }
 
     return Responses.Json({jwt:await jwt.body}, 202) 
 }
